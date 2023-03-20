5bd93da32959228d599040672a9bd737a4b95ec6
Danny <dprakonekham@gmail.com>
2023-03-19 21:18:52 -0700

    fixed issue where the div was growing when text content was introduced

--- script.js
+++ script.js
@@ -5,14 +5,16 @@ const gameBoard = (function () {
         for(let i = 0; i < 3; i++){
             for(let j = 0; j < 3; j++){
                 const gridElement = document.createElement("grid-" + i.toString() + j.toString());
+                gridElement.style.display = "flex";
+                gridElement.style.alignItems = "center";
+                gridElement.style.justifyContent = "center";
                 gridElement.style.backgroundColor = "black";
-                gridElement.style.border = "thick solid purple";
                 gridElement.style.color = "white";
-
+                gridElement.style.border = "1px solid green";
                 //Add event listener to all of the elements
                 gridElement.addEventListener(`click`, function(e){
                     gridElement.textContent = "x";
-                })
+                });
                 gameContainer.appendChild(gridElement);         
             }
         }
@@ -21,3 +23,12 @@ const gameBoard = (function () {
 })();
 
 gameBoard.createBoard();
+
+const resetButton = document.getElementById("rButton");
+resetButton.addEventListener(`click`, function(e){
+    //For every child of gameContainer, set the text content of every node to empty
+    var children = gameContainer.children;
+    for(let i = 0; i < children.length; i++){
+        children[i].textContent = " ";
+    }
+});
