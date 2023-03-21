384da6d7f035c105e72e6c0c8c4cf7cd73d62dfe
Danny <dprakonekham@gmail.com>
2023-03-20 17:01:39 -0700

    implemented opponent code

--- script.js
+++ script.js
@@ -15,13 +15,14 @@ const gameBoard = (function () {
                 gridElement.style.backgroundColor = "black";
                 gridElement.style.color = "white";
                 gridElement.style.border = "1px solid green";
-                const event = (e) => {
-                    console.log(e);
+                const clickEvent = (e) => {
                     let player = 1;
                     gridElement.textContent = "X";
                     //make div unclickable
-                    gridElement.removeEventListener(`click`, event);
-                    opponentPlay(i,j,event);
+                    gridElement.style.pointerEvents = "none";
+                    //console.log(i.toString()+j.toString());
+                    console.log(e.target)
+                    opponentPlay(i,j,e);
                     //Check for winner by using location of the cell as the argument
                     /*let result = checkWinner(player,i,j);
                     if(result == false){
@@ -31,7 +32,7 @@ const gameBoard = (function () {
                     }
                     */
                 };
-                gridElement.addEventListener(`click`, event)
+                gridElement.addEventListener(`click`, clickEvent)
                 gameContainer.appendChild(gridElement);
                 boardRow.push(gridElement);       
             }
@@ -43,10 +44,8 @@ const gameBoard = (function () {
         var children = gameContainer.children;
         for(let i = 0; i < children.length; i++){
             children[i].textContent = "";
+            children[i].style.pointerEvents = "auto";
         }
-    }
-    const event = function(e){
-
     }
     return{turnCounter, board, createBoard, resetBoard}
 })();
@@ -78,81 +77,302 @@ function checkWinner(player,xPos,yPos){
     return winner;
 }
 
-function opponentPlay(xPos,yPos,e){
-    console.log(e);
+function opponentPlay(xPos,yPos){
+
     let player = 2;
     //Place depending on where the player last placed
     let legalMove = false;
     //For corners
     if(xPos == 0 && yPos == 0){
-        let randomChoice = Math.floor(Math.random() * 4);
+        let randomChoice = Math.floor(Math.random() * 3);
         if(randomChoice == 0){
-            if(document.getElementById("10").textContent == ""){
-                document.getElementById("10").textContent = "O";
-                document.getElementById("10").removeEventListener(`click`, e);
+            if(document.getElementById("01").textContent == ""){
+                document.getElementById("01").textContent = "O";
+                document.getElementById("01").style.pointerEvents = "none";
                 legalMove = true;
             }
         }else if(randomChoice == 1){
             if(document.getElementById("11").textContent == ""){
                 document.getElementById("11").textContent = "O";
-                document.getElementById("11").removeEventListener(`click`, e);
+                document.getElementById("11").style.pointerEvents = "none";
                 legalMove = true;
             }
         }else{
-            if(document.getElementById("01").textContent == ""){
-                document.getElementById("01").textContent = "O";
-                document.getElementById("01").removeEventListener(`click`, e);
+            if(document.getElementById("10").textContent == ""){
+                document.getElementById("10").textContent = "O";
+                document.getElementById("10").style.pointerEvents = "none";
                 legalMove = true;
             }
         }
-        if(legalMove == false){
-            while(legalMove == false){
-                for(let i = 2; i <= 0; i--){
-                    for(let j = 2; j <= 0; j--){
-                        if(document.getElementById(i.toString()+j.toString()).textContent == ""){
-                            document.getElementById(i.toString()+j.toString()).textContent == "O";
-                            document.getElementById(i.toString()+j.toString()).removeEventListener(`click`, e);
-                            legalMove = true;
-                        }
-                    }
-                }
-            }
-        }
     }else if(xPos == 2 && yPos == 2){
-        let randomChoice = Math.floor(Math.random() * 4);
+        let randomChoice = Math.floor(Math.random() * 3);
         if(randomChoice == 0){
             if(document.getElementById("12").textContent == ""){
                 document.getElementById("12").textContent = "O";
-                document.getElementById("12").removeEventListener(`click`, e);
+                document.getElementById("12").style.pointerEvents = "none";
                 legalMove = true;
             }
         }else if(randomChoice == 1){
             if(document.getElementById("11").textContent == ""){
                 document.getElementById("11").textContent = "O";
-                document.getElementById("11").removeEventListener(`click`, e);
+                document.getElementById("11").style.pointerEvents = "none";
                 legalMove = true;
             }
         }else{
             if(document.getElementById("21").textContent == ""){
                 document.getElementById("21").textContent = "O";
-                document.getElementById("21").removeEventListener(`click`, e);
+                document.getElementById("21").style.pointerEvents = "none";
                 legalMove = true;
             }
         }
-        if(legalMove == false){
-            while(legalMove == false){
-                for(let i = 0; i > 3; i++){
-                    for(let j = 0; j > 3; j++){
-                        if(document.getElementById(i.toString()+j.toString()).textContent == ""){
-                            document.getElementById(i.toString()+j.toString()).textContent == "O";
-                            document.getElementById(i.toString()+j.toString()).removeEventListener(`click`, e);
-                            legalMove = true;
-                        }
+    }else if(xPos == 2 && yPos == 0){
+        let randomChoice = Math.floor(Math.random() * 3);
+        if(randomChoice == 0){
+            if(document.getElementById("10").textContent == ""){
+                document.getElementById("10").textContent = "O";
+                document.getElementById("10").style.pointerEvents = "none";
+                legalMove = true;
+            }
+        }else if(randomChoice == 1){
+            if(document.getElementById("11").textContent == ""){
+                document.getElementById("11").textContent = "O";
+                document.getElementById("11").style.pointerEvents = "none";
+                legalMove = true;
+            }
+        }else{
+            if(document.getElementById("21").textContent == ""){
+                document.getElementById("21").textContent = "O";
+                document.getElementById("21").style.pointerEvents = "none";
+                legalMove = true;
+            }
+        }
+    }else if(xPos == 0 && yPos == 2){
+        let randomChoice = Math.floor(Math.random() * 3);
+        if(randomChoice == 0){
+            if(document.getElementById("01").textContent == ""){
+                document.getElementById("01").textContent = "O";
+                document.getElementById("01").style.pointerEvents = "none";
+                legalMove = true;
+            }
+        }else if(randomChoice == 1){
+            if(document.getElementById("11").textContent == ""){
+                document.getElementById("11").textContent = "O";
+                document.getElementById("11").style.pointerEvents = "none";
+                legalMove = true;
+            }
+        }else{
+            if(document.getElementById("12").textContent == ""){
+                document.getElementById("12").textContent = "O";
+                document.getElementById("12").style.pointerEvents = "none";
+                legalMove = true;
+            }
+        }
+
+    }else if(xPos == 0 && yPos == 1){
+        let randomChoice = Math.floor(Math.random() * 5);
+        if(randomChoice == 0){
+            if(document.getElementById("00").textContent == ""){
+                document.getElementById("00").textContent = "O";
+                document.getElementById("00").style.pointerEvents = "none";
+                legalMove = true;
+            }
+        }else if(randomChoice == 1){
+            if(document.getElementById("10").textContent == ""){
+                document.getElementById("10").textContent = "O";
+                document.getElementById("10").style.pointerEvents = "none";
+                legalMove = true;
+            }
+        }else if(randomChoice == 2){
+            if(document.getElementById("11").textContent == ""){
+                document.getElementById("11").textContent = "O";
+                document.getElementById("11").style.pointerEvents = "none";
+                legalMove = true;
+            }
+        }else if(randomChoice == 3){
+            if(document.getElementById("12").textContent == ""){
+                document.getElementById("12").textContent = "O";
+                document.getElementById("12").style.pointerEvents = "none";
+                legalMove = true;
+            }
+        }else{
+            if(document.getElementById("02").textContent == ""){
+                document.getElementById("02").textContent = "O";
+                document.getElementById("02").style.pointerEvents = "none";
+                legalMove = true;
+            }
+        }
+
+    }else if(xPos == 1 && yPos == 0){
+        let randomChoice = Math.floor(Math.random() * 5);
+        if(randomChoice == 0){
+            if(document.getElementById("00").textContent == ""){
+                document.getElementById("00").textContent = "O";
+                document.getElementById("00").style.pointerEvents = "none";
+                legalMove = true;
+            }
+        }else if(randomChoice == 1){
+            if(document.getElementById("01").textContent == ""){
+                document.getElementById("01").textContent = "O";
+                document.getElementById("01").style.pointerEvents = "none";
+                legalMove = true;
+            }
+        }else if(randomChoice == 2){
+            if(document.getElementById("11").textContent == ""){
+                document.getElementById("11").textContent = "O";
+                document.getElementById("11").style.pointerEvents = "none";
+                legalMove = true;
+            }
+        }else if(randomChoice == 3){
+            if(document.getElementById("21").textContent == ""){
+                document.getElementById("21").textContent = "O";
+                document.getElementById("21").style.pointerEvents = "none";
+                legalMove = true;
+            }
+        }else{
+            if(document.getElementById("20").textContent == ""){
+                document.getElementById("20").textContent = "O";
+                document.getElementById("20").style.pointerEvents = "none";
+                legalMove = true;
+            }
+        }
+
+    }else if(xPos == 1 && yPos == 1){
+        let randomChoice = Math.floor(Math.random() * 8);
+        if(randomChoice == 0){
+            if(document.getElementById("00").textContent == ""){
+                document.getElementById("00").textContent = "O";
+                document.getElementById("00").style.pointerEvents = "none";
+                legalMove = true;
+            }
+        }else if(randomChoice == 1){
+            if(document.getElementById("01").textContent == ""){
+                document.getElementById("01").textContent = "O";
+                document.getElementById("01").style.pointerEvents = "none";
+                legalMove = true;
+            }
+        }else if(randomChoice == 2){
+            if(document.getElementById("02").textContent == ""){
+                document.getElementById("02").textContent = "O";
+                document.getElementById("02").style.pointerEvents = "none";
+                legalMove = true;
+            }
+        }else if(randomChoice == 3){
+            if(document.getElementById("10").textContent == ""){
+                document.getElementById("10").textContent = "O";
+                document.getElementById("10").style.pointerEvents = "none";
+                legalMove = true;
+            }
+        }else if(randomChoice == 4){
+            if(document.getElementById("12").textContent == ""){
+                document.getElementById("12").textContent = "O";
+                document.getElementById("12").style.pointerEvents = "none";
+                legalMove = true;
+            }
+        }else if(randomChoice == 5){
+            if(document.getElementById("20").textContent == ""){
+                document.getElementById("20").textContent = "O";
+                document.getElementById("20").style.pointerEvents = "none";
+                legalMove = true;
+            }
+        }else if(randomChoice == 6){
+            if(document.getElementById("21").textContent == ""){
+                document.getElementById("21").textContent = "O";
+                document.getElementById("21").style.pointerEvents = "none";
+                legalMove = true;
+            }
+        }else{
+            if(document.getElementById("22").textContent == ""){
+                document.getElementById("22").textContent = "O";
+                document.getElementById("22").style.pointerEvents = "none";
+                legalMove = true;
+            }
+        }
+
+    }else if(xPos == 1 && yPos == 2){
+        let randomChoice = Math.floor(Math.random() * 5);
+        if(randomChoice == 0){
+            if(document.getElementById("02").textContent == ""){
+                document.getElementById("02").textContent = "O";
+                document.getElementById("02").style.pointerEvents = "none";
+                legalMove = true;
+            }
+        }else if(randomChoice == 1){
+            if(document.getElementById("01").textContent == ""){
+                document.getElementById("01").textContent = "O";
+                document.getElementById("01").style.pointerEvents = "none";
+                legalMove = true;
+            }
+        }else if(randomChoice == 2){
+            if(document.getElementById("11").textContent == ""){
+                document.getElementById("11").textContent = "O";
+                document.getElementById("11").style.pointerEvents = "none";
+                legalMove = true;
+            }
+        }else if(randomChoice == 3){
+            if(document.getElementById("21").textContent == ""){
+                document.getElementById("21").textContent = "O";
+                document.getElementById("21").style.pointerEvents = "none";
+                legalMove = true;
+            }
+        }else{
+            if(document.getElementById("22").textContent == ""){
+                document.getElementById("22").textContent = "O";
+                document.getElementById("22").style.pointerEvents = "none";
+                legalMove = true;
+            }
+        }
+
+    }else if(xPos == 2 && yPos == 1){
+        let randomChoice = Math.floor(Math.random() * 5);
+        if(randomChoice == 0){
+            if(document.getElementById("20").textContent == ""){
+                document.getElementById("20").textContent = "O";
+                document.getElementById("20").style.pointerEvents = "none";
+                legalMove = true;
+            }
+        }else if(randomChoice == 1){
+            if(document.getElementById("10").textContent == ""){
+                document.getElementById("10").textContent = "O";
+                document.getElementById("10").style.pointerEvents = "none";
+                legalMove = true;
+            }
+        }else if(randomChoice == 2){
+            if(document.getElementById("11").textContent == ""){
+                document.getElementById("11").textContent = "O";
+                document.getElementById("11").style.pointerEvents = "none";
+                legalMove = true;
+            }
+        }else if(randomChoice == 3){
+            if(document.getElementById("12").textContent == ""){
+                document.getElementById("12").textContent = "O";
+                document.getElementById("12").style.pointerEvents = "none";
+                legalMove = true;
+            }
+        }else{
+            if(document.getElementById("22").textContent == ""){
+                document.getElementById("22").textContent = "O";
+                document.getElementById("22").style.pointerEvents = "none";
+                legalMove = true;
+            }
+        }
+    }
+
+    console.log(legalMove);
+    if(!legalMove){
+        console.log("hey")
+        let move = 1;
+        while(move == 1){
+            for(let i = 0; i < 3; i++){
+                for(let j = 0; j < 3; j++){
+                    if(document.getElementById(i.toString()+j.toString()).textContent == ""){
+                        document.getElementById(i.toString()+j.toString()).textContent = "0";
+                        document.getElementById(i.toString()+j.toString()).style.pointerEvents = "none";
+                        move = 2;
+                        j = i = 3;
                     }
                 }
             }
         }
-
     }
 
     //Check for winner
